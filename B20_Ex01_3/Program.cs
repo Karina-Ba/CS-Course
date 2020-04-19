07c3bde856d9c6609df70af4e87b9a23cdbe632d
Karina Batmanishvili <karinaba@mta.ac.il>
2020-04-19 23:25:59 +0300

    Made changes and added 3rd q

---
+++ B20_Ex01_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace B20_Ex01_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            InitProgram();
+        }
+
+        public static void InitProgram()
+        {
+            int heightOfSandClock = getSandClockHeight();
+            Console.WriteLine("Here is your chosen sand clock:");
+            B20_Ex01_2.Program.PrintSandClock(heightOfSandClock);
+        }
+
+        private static int getSandClockHeight()
+        {
+            Console.WriteLine("Please enter the height you want for the sand clock:");
+            string heightOfSandClockStr = Console.ReadLine();
+            int heightOfSandClockInt;
+            bool v_heightStringToInt = Int32.TryParse(heightOfSandClockStr, out heightOfSandClockInt); ;
+
+            while (!v_heightStringToInt || heightOfSandClockInt <= 0)
+            {
+                Console.WriteLine(
+@"Unable to create sand clock with that input.
+Please enter a valid positive number for the height of the sand clock:"); //Change to format
+                heightOfSandClockStr = Console.ReadLine();
+                v_heightStringToInt = Int32.TryParse(heightOfSandClockStr, out heightOfSandClockInt);
+            }
+
+            return heightOfSandClockInt;
+        }
+    }
+}
